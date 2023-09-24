eb3ea745afaa0d9130d8fdc1ff878bfc52ddaf10
Kirill <misterkirill666@gmail.com>
2023-09-24 15:40:23 +0500

    Update user.rs

--- src/models/user.rs
+++ src/models/user.rs
@@ -4,6 +4,7 @@ use serde::{Deserialize, Serialize};
 pub struct User {
     #[sqlx(rename = "accountID")]
     pub account_id: i32,
+    #[sqlx(rename = "userName")]
     pub username: String,
     pub password: String,
     pub token: String,
